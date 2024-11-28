54495c365490971d8143ee70b6b2bf323318efeb
Kaio356 <goiskaio46@gmail.com>
2024-11-28 12:18:24 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'COPA do mundo',
+    'Quando o Brasil Ganhou a copa?',
+    'Em 2002'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'Paris'
+    'COpa do Mundo',
+    'Qual o ultimo melhor jogador da copa?',
+    'Mbappé'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Copa do mundo',
+    'QUal o dia da final da copa de 2022',
+    '18/12'
 )
 
 criaCartao (
-    'CSS',
-    'Como mudar a cor de fundo com CSS?',
-    'Use background-color'
+    'Tema Champions League',
+    'Quem ganhou a champions league 2015?',
+    'Barcelona'
 )
 
 criaCartao (
-    'JavaScript',
-    'O que é JavaScript?',
-    'Linguagem para adicionar interatividade em páginas web'
+    'Champions League',
+    'Quem é o maior artilheiro?',
+    'Cristiano Ronaldo'
 )
 
 criaCartao (
-    'CSS',
-    'O que é CSS?',
-    'É uma linguagem para estilizar páginas HTML'
+    'Champions League',
+    'Quantas champions tem o psg?',
+    'Nenhuma'
 )
